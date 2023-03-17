76760bc8378a1aed3daa6e97b3428f7718522a76
Aribisala Boluwatife <boluwatifearibisala1@gmail.com>
2023-03-16 20:42:24 -0700

    4-puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+int main(void)
+
+
+/**
+ * main - prints "Programming is like bulding a multilingual
+ * puzzle, followed by anew line."
+ *
+ * return 0.
+ */
+
+
+	
+
+{
+    puts("\"Programming is like bulding a multilingual puzzle");
+
+    return(0);    	
+}
+
