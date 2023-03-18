6582f4243aa1b4aa0e16a3a505ff668475cab530
Aribisala Boluwatife <boluwatifearibisala1@gmail.com>
2023-03-17 18:23:51 -0700

    New files added for whilke loops and variables

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main -Entry point
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+char lup, e, q;
+e = 'e'
+q = 'q'
+for (lup = 'a'; lup <= 'z'; lup++)
+{
+if (lup != e && lup != q)
+putchar(lup);
+}
+puchar('\n');
+return (0);
+}
