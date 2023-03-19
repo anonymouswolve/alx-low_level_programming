4e75920f515691e3518b3315861e7c64f3bdb1af
Aribisala Boluwatife <boluwatifearibisala1@gmail.com>
2023-03-19 15:38:05 -0700

    New files added for whilke loops and variables

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,14 +1,11 @@
- #include <stdio.h>
-
-/**main - prints "and that piece of art is useful" -Dora Korpar, 2015-10-19",
- * followed by a newline, to standard error.
- * Return: Always 1.
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * main - Entry point
+ * Return: Always 0 (Success)
  */
-
-
- int main(void)
- {
-
-         write(2, "and  that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
-         return(1);
- }
+int main(void)
+{
+write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+return (1);
+}
