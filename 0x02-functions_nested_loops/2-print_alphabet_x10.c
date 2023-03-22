d332aae4493cf1883d60e93ec5de2076d2724e38
Aribisala Boluwatife <boluwatifearibisala1@gmail.com>
2023-03-21 18:21:48 -0700

    wrote a script to print out the alphabet 10 times

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - make alphabets appear 10 times
+ *
+ * Return: Always 0 (Success)
+ */
+void print_alphabet_x10(void)
+{
+	char c;
+	int i = 0;
+
+	while (i <= 9)
+	{
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		_putchar(c);
+		_putchar('\n');
+	}
+		i++;
+	}
+}
