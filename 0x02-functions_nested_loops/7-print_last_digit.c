a08a5914745b4d9e566381e5b307f5cec95f244d
Aribisala Boluwatife <boluwatifearibisala1@gmail.com>
2023-03-21 21:57:46 -0700

    updated

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * print_last_digit - print the last digit of a number
+ * print_last_digit - Prints the last digit of a number
  * @n: The number to be treated
  * Return: Value of last digit of a number
  */
