9a8ab30cc2ec13f119a1f8fc069d30f040b3c711
Aribisala Boluwatife <boluwatifearibisala1@gmail.com>
2023-03-24 16:50:25 -0700

    updated

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * Print_triangle - Prints a triangle of squares according parameter
+ * print_triangle - Prints a triangle of squares according parameter
  * @size: The size of the squares traiangle
  * Return: empty
  */
