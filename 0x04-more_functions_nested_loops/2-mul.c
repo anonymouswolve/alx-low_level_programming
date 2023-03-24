aa1b4757fede965d8ea7e015a048b312dd72dd3d
Aribisala Boluwatife <boluwatifearibisala1@gmail.com>
2023-03-24 12:07:03 -0700

    Wrote a function that multiplies two integers

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+* mul - multiples of two integers
+* @x: The first number to multiply
+* @y: The second number to multipl with the first number
+* Return: The result of multiplication between x and y
+*/
+
+int mul(int x, int y)
+{
+	return(x * y);
+}
