55b41e3166690bc6afd42d7c85d33056a27d5b84
Aribisala Boluwatife <boluwatifearibisala1@gmail.com>
2023-03-27 17:45:58 -0700

    Created a script to swap values of two integers

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * swap_int - swaps ttwo integers values
+ * @a: first integer 
+ * @b: second integer
+ * Retrun: Nothing
+ */
+
+void swap_int(int *a, int *b)
+{
+	int tmp = *a;
+
+	*a = *b;
+	*b = tmp;
+}
